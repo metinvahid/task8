3a09a10350c7525ec754a4ef77e9b7ef6582d143
metinvahid <matinav@code.edu.az>
2024-01-06 00:04:07 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hey");
+        }
+    }
+}
